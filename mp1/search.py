591f80c24bffbd48a438293e370b80ef0877d53c
SwordTechCorp <cxyddsyx@hotmail.com>
2024-03-14 23:22:18 +0800

    [New]Single_Goal_Finished

--- mp1/search.py
+++ mp1/search.py
@@ -94,65 +94,83 @@ def dfs(maze):
 def _prSearch(maze,costFun, objectives ):
 
     class comparableCord():
-        def __init__(self, s:tuple) -> None:
+        def __init__(self, s:tuple, parent = None) -> None:
             self.cord = s
+            self.parent = parent
+            if (parent != None):
+                self.dist = parent.dist + 1
+            else:
+                self.dist = 0
+            self.cost = costFun(self)
         def __lt__(self, that) -> bool:
-            costSelf = costFun(self.cord)
-            costThat = costFun(that.cord)
-            if costSelf < costThat: return True
-            if (costSelf == costThat) and (self.cord[0] < that.cord[0]): return True
-            if (costSelf == costThat) and (self.cord[0] == that.cord[0])and (self[1].cord < that[1].cord): return True
-            return False  
+            if self.cost < that.cost: return True
+            if (self.cost == that.cost) and (self.cord[0] < that.cord[0]): return True
+            if (self.cost == that.cost) and (self.cord[0] == that.cord[0]) and (self.cord[1] < that.cord[1]): return True
+            return False
+        def __hash__(self) -> int:
+            return hash(self.cord)
+        def __eq__(self, that) -> bool:
+            if (that == None): return False
+            return self.cord == that.cord
     class priorQueue:
         def __init__(self) -> None:
             self.queue = []
-            pass
-        def pop(self)->tuple:
-            return heappop(self.queue).cord
-        
-        def push(self,cord:tuple) -> None:
-            heappush(self.queue,comparableCord(cord))
             return
-        def min(self) -> tuple:
-            return self.queue[0].cord
+
+        def pop(self)->comparableCord:
+            obj = heappop(self.queue)
+            return obj
+        
+        def push(self,cord:comparableCord) -> None:
+            heappush(self.queue, cord)
+            return
+        
+        def min(self) -> comparableCord:
+            return self.queue[0]
+        
         def isEmpty(self) -> bool:
             return (self.queue.__len__() == 0)
-
    
     queue = priorQueue()
-    parents = dict()
-    curNode = maze.getStart()
+    explored = set()
+    curNode = comparableCord(maze.getStart())
     queue.push(curNode)
-    parents[curNode] = None
+    explored.add(curNode)
     while(not queue.isEmpty()):
         curNode = queue.pop()
-        if curNode in objectives:
+        if curNode.cord in objectives:
             break
-        for node in maze.getNeighbors(curNode[0], curNode[1]):
-            if node not in parents.keys():
-                queue.push(node)
-                parents[node] = curNode
-    if (curNode not in objectives):
+        for node in maze.getNeighbors(curNode.cord[0], curNode.cord[1]):
+            nextNode = comparableCord(node,curNode)
+            if nextNode not in explored:
+                queue.push(nextNode)
+        explored.add(curNode)
+    if (curNode.cord not in objectives):
         return [], 0
     else:
         path = []
         while(curNode != None):
-            path.append(curNode)
-            curNode = parents[curNode]
+            path.append(curNode.cord)
+            curNode = curNode.parent
         path.reverse()
-        return path, len(parents.keys())
+        return path, len(explored)
     # TODO: Write your code here
     # return path, num_states_explored
 
 def greedy(maze):
     objectives = maze.getObjectives()
     target = objectives[-1]
-    def heurf(point):
-        return abs(target[0]-point[0]) + abs(target[1]-point[1])
-    return _prSearch(maze,heurf,objectives)
+    def cost(point):
+        return abs(target[0]-point.cord[0]) + abs(target[1]-point.cord[1])
+    return _prSearch(maze,cost,objectives)
 
 
 def astar(maze):
+    objectives = maze.getObjectives()
+    target = objectives[-1]
+    def cost(point):
+        return abs(target[0]-point.cord[0]) + abs(target[1]-point.cord[1]) + point.dist
+        #return point.dist
+    return _prSearch(maze,cost,objectives)
     # TODO: Write your code here
     # return path, num_states_explored
-    return [], 0
