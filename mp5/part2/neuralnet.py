607f61c1718da07e2899d958b171b11a64fb9baf
Zhirong Chen <zhirong.20@intl.zju.edu.cn>
2024-05-25 15:12:02 +0800

    upload part 2

--- mp5/part2/neuralnet.py
+++ mp5/part2/neuralnet.py
@@ -22,6 +22,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from tqdm import tqdm
 
 
 class NeuralNet(nn.Module):
@@ -44,21 +45,11 @@ class NeuralNet(nn.Module):
         """
         super(NeuralNet, self).__init__()
         self.loss_fn = loss_fn
-        raise NotImplementedError("You need to write this part!")
+        self.layer1 = nn.Linear(in_size, 32)
+        self.layer2 = nn.Linear(32, out_size)
+        self.optimizer = optim.Adam(self.parameters(), lr=lrate)
 
-    # def set_parameters(self, params):
-    #     """ Sets the parameters of your network.
 
-    #     @param params: a list of tensors containing all parameters of the network
-    #     """
-    #     raise NotImplementedError("You need to write this part!")
-
-    # def get_parameters(self):
-    #     """ Gets the parameters of your network.
-
-    #     @return params: a list of tensors containing all parameters of the network
-    #     """
-    #     raise NotImplementedError("You need to write this part!")
 
     def forward(self, x):
         """Performs a forward pass through your neural net (evaluates f(x)).
@@ -66,8 +57,9 @@ class NeuralNet(nn.Module):
         @param x: an (N, in_size) Tensor
         @return y: an (N, out_size) Tensor of output from the network
         """
-        raise NotImplementedError("You need to write this part!")
-        return torch.ones(x.shape[0], 1)
+        x = F.relu(self.layer1(x))
+        x = self.layer2(x)
+        return x
 
     def step(self, x, y):
         """
@@ -77,8 +69,17 @@ class NeuralNet(nn.Module):
         @param y: an (N,) Tensor
         @return L: total empirical risk (mean of losses) at this timestep as a float
         """
-        raise NotImplementedError("You need to write this part!")
-        return 0.0
+        self.optimizer.zero_grad()  # Reset gradients
+        y_pred = self.forward(x)    # Compute predictions
+        loss = self.loss_fn(y_pred, y)  # Compute loss
+        loss.backward()             # Compute gradients
+        self.optimizer.step()       # Update parameters
+        return loss.item()          # Return the loss value
+    
+    def to(self, device):
+        self.layer1 = self.layer1.to(device)
+        self.layer2 = self.layer2.to(device)
+        return super(NeuralNet, self).to(device)
 
 
 def fit(train_set, train_labels, dev_set, n_iter, batch_size=100):
@@ -86,7 +87,7 @@ def fit(train_set, train_labels, dev_set, n_iter, batch_size=100):
 
     @param train_set: an (N, in_size) Tensor
     @param train_labels: an (N,) Tensor
-    @param dev_set: an (M,) Tensor
+    @param dev_set: an (M, in_size) Tensor
     @param n_iter: an int, the number of epoches of training
     @param batch_size: size of each batch to train on. (default 100)
 
@@ -97,5 +98,34 @@ def fit(train_set, train_labels, dev_set, n_iter, batch_size=100):
     @return yhats: an (M,) NumPy array of binary labels for dev_set
     @return net: a NeuralNet object
     """
-    raise NotImplementedError("You need to write this part!")
-    return [], [], None
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    in_size = train_set.shape[1]
+    out_size = 2  # Assuming binary classification problem
+    lrate = 0.001
+    loss_fn = nn.CrossEntropyLoss()
+
+    net = NeuralNet(lrate, loss_fn, in_size, out_size).to(device)
+    train_set = train_set.to(device)
+    train_labels = train_labels.to(device)
+    dev_set = dev_set.to(device)
+    mean = train_set.mean()
+    std = train_set.std()
+    train_set = (train_set - mean) / std
+    dev_set = (dev_set - mean) / std 
+    losses = []
+
+    for epoch in tqdm(range(n_iter)):
+        for i in range(0, len(train_set), batch_size):
+            batch_x = train_set[i:i + batch_size]
+            batch_y = train_labels[i:i + batch_size]
+            loss = net.step(batch_x, batch_y)
+            losses.append(loss)
+        if epoch % 20 == 0:
+            print(f"Epoch {epoch + 1}/{n_iter}, Loss: {losses[-1]}")
+
+    # Evaluation on development set
+    dev_preds = net(dev_set)
+    yhats = torch.argmax(dev_preds, dim=1).cpu().numpy()  # Convert to NumPy array for binary labels
+
+    return losses, yhats, net
+
